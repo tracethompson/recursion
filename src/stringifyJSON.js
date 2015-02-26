558be542fdc1e680d3c26ea39cf70e6f444ebb3e
Trace Thompson <tracethmpsn@gmail.com>
2015-02-26 13:24:08 -0800

    Easy part of stringify done --- now to array and object

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,47 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  //Are you empty?
+  if (obj === null){
+  	return "null";
+  } 
+  else if (obj === undefined){
+  	return "undefined";
+  }
+
+  //Are you a number boolean or string?
+  else if(typeof(obj) === "string"){
+  	return '"' + obj + '"';
+  } 
+  else if (typeof(obj) === "number") {
+    return String(obj);
+  } 
+  else if (typeof(obj) === "boolean"){
+    return '' + obj + ''
+  }
+
+
+  // Are you an array or an object?
+  else if (Array.isArray(obj)){
+    var temp="",
+    len=obj.length;
+
+    for (var i=0;i<len;i++){
+      temp=temp.concat(stringifyJSON(obj[i])+',');
+    }
+    return '['+temp.slice(0,temp.length-1)+']';
+  } 
+
+
+  else if (typeof(obj)==="object"){   // and object
+    var temp="";
+    for (var prop in obj){
+      if (obj[prop] !== undefined && typeof(obj[prop])!=="function"){
+        temp=temp.concat(stringifyJSON(prop)+':'+stringifyJSON(obj[prop])+',');
+      }
+    }
+    return '{'+temp.slice(0,temp.length-1)+'}'; // deleting trailing ','
+  }
+
 };
