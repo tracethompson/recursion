eee99e1c43ccb75521d8d212fb6ed54171b49960
Trace Thompson <tracethmpsn@gmail.com>
2015-02-26 19:37:49 -0800

    Implemeneted underscore and cleaned a lil bit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -10,14 +10,14 @@ function getElementsByClassName(className){
   var getElementByClassName = function(nodes){
   	for (var i = 0; i < nodes.length; i++){
   		if (nodes[i].classList !== undefined){    //makes sure we skip over nodes without a class name
-  			if (nodes[i].classList.contains(className)){
+  			if (_.contains(nodes[i].classList, className)){
   			result.push(nodes[i]); //push the nodes with the given className to the result array
   		  }
   	  }
     getElementByClassName(nodes[i].childNodes); //repeat process for the child nodes of each parent node
     }
   }  
-  var body = document.body;
-  getElementByClassName([body]);
+  var body = [document.body];
+  getElementByClassName(body);
   return result;
 };
